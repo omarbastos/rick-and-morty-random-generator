735fcdc1f689bb4b92eb1e0127d865a6c2ea9322
Omar Jesus Hernandez Bastos <omarsro@gmail.com>
2020-10-05 08:51:49 -0400

    Fixed some bugs about travis

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,9 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
   target: 'static',
+  router: {
+    base: '/rick-and-morty-random-generator/',
+  },
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
